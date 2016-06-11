0499cf75fa022b644f8298c9def24834cbb66412
saad <sa9916@cs.ship.edu>
2016-06-11 01:50:01 -0400

    the invoker calss is uptaed and invokerbuilder class are created
    the ActionListnener is added in invoker class as well as the set methods  Also the invoker builder are created

--- src/ui/command/Invoker.java
+++ src/ui/command/Invoker.java
@@ -5,11 +5,31 @@ import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Invoker 
+public class Invoker implements ActionListener
 {
 
 	private JFrame frame;
+	Command reloadCm;
+	Command AcquireCm ; 
+	Command MoveCm ;
+	Command DropCm ;
+	Command TurnEastCm ;
+	Command TurnSouthCm ; 
+	Command TurnNourthCm ; 
+	Command TurnWestCm ; 
+	Command AttackCm ; 
+	JButton reload ; 
+	JButton acquire ; 
+	JButton turn ; 
+	JButton move ; 
+	JButton drop ; 
+	JButton east ; 
+	JButton north ; 
+	JButton west ; 
+	JButton south ; 
 
 	/**
 	 * Launch the application.
@@ -43,31 +63,124 @@ public class Invoker
 		frame.getContentPane().add(panel, BorderLayout.SOUTH);
 		panel.setLayout(new GridLayout(1, 0, 0, 0));
 		
-		JButton reload = new JButton("Reload");
+		reload = new JButton("Reload");
 		panel.add(reload);
+		reload.addActionListener(this);
 		
-		JButton acquire = new JButton("Acquire");
+		acquire = new JButton("Acquire");
 		panel.add(acquire);
+		acquire.addActionListener(this);
 		
-		JButton turn = new JButton("Turn");
+	    turn = new JButton("Turn");
 		panel.add(turn);
+		turn.addActionListener(this);
 		
-		JButton move = new JButton("Move");
+		move = new JButton("Move");
 		panel.add(move);
+		move.addActionListener(this);
 		
-		JButton drop = new JButton("Drop");
+		drop = new JButton("Drop");
 		panel.add(drop);
+		drop.addActionListener(this);
 		
-		JButton newButton = new JButton("East");
-		frame.getContentPane().add(newButton, BorderLayout.EAST);
+		east = new JButton("East");
+		frame.getContentPane().add(east, BorderLayout.EAST);
+		east.addActionListener(this);
 		
-		JButton north = new JButton("North");
+		north = new JButton("North");
 		frame.getContentPane().add(north, BorderLayout.NORTH);
+		north.addActionListener(this);
 		
-		JButton west = new JButton("West");
+		west = new JButton("West");
 		frame.getContentPane().add(west, BorderLayout.WEST);
+		west.addActionListener(this);
 		
-		JButton south = new JButton("South");
+		south = new JButton("South");
 		frame.getContentPane().add(south, BorderLayout.CENTER);
+		south.addActionListener(this);
+	}
+	
+	/**
+	 * Set Methods to set Commands 
+	 * @author Saad
+	 */
+	
+	public void setRelaod(Command reload)
+	{
+		reloadCm = reload ; 
+	}
+	public void setAcquire(Command aquire)
+	{
+		AcquireCm = aquire ; 
+	}
+	public void setDrop(Command drop)
+	{
+		DropCm = drop ; 
+	}
+	public void setTurnEast(Command east)
+	{
+		TurnEastCm = east ; 
+	}
+	public void setTurnWest(Command west)
+	{
+		TurnWestCm = west ; 
+	}
+	public void setTurnNorth(Command north)
+	{
+		TurnNourthCm = north ; 
+	}
+	public void setTurnSouth(Command south)
+	{
+		TurnSouthCm = south ; 
+	}
+	public void setMove(Command move)
+	{
+		MoveCm = move ; 
+	}
+	public void setAttack(Command attack)
+	{
+		AttackCm = attack ; 
+	}
+	
+    /**
+     * Action Listener to handle each action for buttons 
+     * @author Saad
+     */
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		if(event.getSource()== reload)
+		{
+			reloadCm.execute();
+		}
+		else if(event.getSource()== acquire)
+		{
+			AcquireCm.execute();
+		}
+		else if(event.getSource()== move)
+		{
+			MoveCm.execute();
+		}
+		else if(event.getSource()== drop)
+		{
+			DropCm.execute();
+		}
+		else if(event.getSource()== east)
+		{
+			TurnEastCm.execute();
+		}
+		else if(event.getSource()== west)
+		{
+			TurnWestCm.execute();
+		}
+		else if(event.getSource()== south)
+		{
+			TurnSouthCm.execute();
+		}
+		else if(event.getSource()== north)
+		{
+			TurnNourthCm.execute();
+		}
+		
+		
 	}
 }
---
+++ src/ui/command/InvokerBuilder.java
@@ -0,0 +1,29 @@
+package ui.command;
+
+import lifeform.LifeForm;
+
+public class InvokerBuilder {
+	/**
+	 * get Invoker method 
+	 * @param life
+	 * @return invoker 
+	 * @author Saad
+	 */
+	
+	public Invoker getInvoker(LifeForm life)
+	{
+		Invoker invoker = new Invoker();
+		
+		invoker.setRelaod(new ReloadCommand(life));
+		invoker.setAcquire(new AcquireCommand(life));
+		invoker.setDrop(new DropCommand(life));
+		invoker.setMove(new MoveCommand(life));
+		invoker.setTurnEast(new TurnEastCommand(life));
+		invoker.setTurnNorth(new TurnNorthCommand(life));
+		invoker.setTurnSouth(new TurnSouthCommand(life));
+		invoker.setTurnWest(new TurnWestCommand(life));
+		
+		return invoker ; 
+	}
+
+}
